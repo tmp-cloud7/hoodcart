649d0ce8ba98c7f6dd12e59cdd6e88132e3358e0
taio-ops <tayopopoola92@gmail.com>
2025-10-15 15:07:45 +0100

    first commit

---
+++ shop_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import response
+
+# Create your views here.
+
+@api_view(["GET"])
+def products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
