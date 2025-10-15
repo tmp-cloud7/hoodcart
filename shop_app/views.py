0b4df492531acd6ae261b78f960d2f5c2948b556
taio-ops <tayopopoola92@gmail.com>
2025-10-15 15:35:56 +0100

    Modified Response import from rest_framework in views @ shop_app

--- shop_app/views.py
+++ shop_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework import response
+from rest_framework.response import Response
 
 # Create your views here.
 
