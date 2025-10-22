42c64cb350f8ab6f4f98cb4261111bdfabcdb4dd
taio-ops <tayopopoola92@gmail.com>
2025-10-22 11:21:53 +0100

    Modified shop_app.serializers.py

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -23,6 +23,7 @@ class CartSerializer(serializers.ModelSerializer):
         fields = ["id", "cart_code", "created_at", "modified_at"]
 
 class SimpleCartSerializer(serializers.ModelSerializer):
+    num_of_items = serializers.SerializerMethodField()
     class Meta:
         model = Cart
         fields = ["id", "cart_code", "num_of_items"]
