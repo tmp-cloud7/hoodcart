ae789cd118d3b78bbc57ec4ca1b08aa3cbcc78eb
taio-ops <tayopopoola92@gmail.com>
2025-11-05 16:24:03 +0100

    Allowed descriptive commit message

--- hoodcart/settings.py
+++ hoodcart/settings.py
@@ -65,6 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://newshop-k2zg.onrender.com"
 ]
 
 ROOT_URLCONF = 'hoodcart.urls'
